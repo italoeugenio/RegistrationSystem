a1ead592447ea0b46d8ff591fc230b7111b7a238
Ítalo Santana <italoeugenio539@gmail.com>
2024-12-19 15:48:23 -0300

    Adding AgeValidator

--- challange/src/user/User.java
+++ challange/src/user/User.java
@@ -12,7 +12,21 @@ public class User extends Person {
 
     public static void enterInformations(){
         Scanner scanner = new Scanner(System.in);
+        System.out.println();
 
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
 
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+
+        System.out.print("Age: ");
+        int age = scanner.nextInt();
+        while(!AgeValidator.ageValidator(age)){
+            System.out.print("Please,enter a valid age: ");
+        }
+
+        System.out.println("Height: ");
+        float height = scanner.nextFloat();
     }
 }
--- challange/src/utils/AgeValidator.java
+++ challange/src/utils/AgeValidator.java
@@ -1,4 +1,10 @@
 package utils;
 
 public class AgeValidator {
+    public static boolean ageValidator(int age){
+        if(age >= 0 && age <= 150){
+            return true;
+        }
+        return false;
+    }
 }
