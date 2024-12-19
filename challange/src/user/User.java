23caec36bdfbca0fe0e236ea6a38e4b4c4387157
Ítalo Santana <italoeugenio539@gmail.com>
2024-12-19 16:28:36 -0300

    separating the functions

--- challange/src/user/User.java
+++ challange/src/user/User.java
@@ -10,23 +10,41 @@ public class User extends Person {
         super(name, email, age, height);
     }
 
-    public static void enterInformations(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println();
+    public static void userInformations(){
+        enterName();
+        enterEmail();
+        enterAge();
+        enterHeight();
+    }
 
+    public static void enterName(){
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Name: ");
         String name = scanner.nextLine();
+        scanner.close();
+    }
 
-        System.out.print("Email: ");
-        String email = scanner.nextLine();
+    public static void enterEmail(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+        scanner.close();
+    }
 
+    public static void enterAge(){
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Age: ");
         int age = scanner.nextInt();
         while(!AgeValidator.ageValidator(age)){
             System.out.print("Please,enter a valid age: ");
         }
+        scanner.close();
+    }
 
+    public static void enterHeight(){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Height: ");
         float height = scanner.nextFloat();
+        scanner.close();
     }
 }
