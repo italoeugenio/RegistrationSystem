30c8eefd063291a2aaa634658ee38419f9cfd9c1
Ítalo Santana <italoeugenio539@gmail.com>
2024-12-19 14:42:11 -0300

    Adding the function that reads the forms.txt

---
+++ challange/src/application/Main.java
@@ -0,0 +1,9 @@
+package application;
+
+import utils.form.FormReader;
+
+public class Main {
+    public static void main(String[] args) {
+        FormReader.formReader();
+    }
+}
---
+++ challange/src/utils/form/FormReader.java
@@ -0,0 +1,19 @@
+package utils.form;
+
+import java.io.BufferedReader;
+import java.io.File;
+
+public class FormReader {
+    public static void formReader (){
+        File file = new File("src/resources/forms.txt");
+        try (java.io.FileReader fr = new java.io.FileReader(file);
+             BufferedReader br = new BufferedReader(fr)) {
+            String line;
+            while((line = br.readLine()) != null){
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
