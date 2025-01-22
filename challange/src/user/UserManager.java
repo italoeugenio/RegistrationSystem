21886c4cbf8c86f93c94e399347f49ef4188aacf
Ítalo Santana <italoeugenio539@gmail.com>
2025-01-22 15:59:44 -0300

    Adding step 3

--- challange/src/user/UserManager.java
+++ challange/src/user/UserManager.java
@@ -4,6 +4,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,9 +17,31 @@ public class UserManager {
         System.out.println();
         System.out.println("--Congratulations---");
         System.out.println("User: " + user.getName() + " added");
+        formWriterUser(user);
     }
 
     public static void formWriterUser(User user){
+        String name = user.getName();
+        String[] parts = user.getName().split(" ");
+        String concatenated = "";
 
+        if(parts.length >= 2){
+            String firstName = parts[0];
+            String secondName = parts[1];
+            concatenated = firstName.toUpperCase() + secondName.toUpperCase();
+        }
+
+        File file = new File("C:\\Users\\italo.santana\\OneDrive - cnj.jus.br\\Área de Trabalho\\Language\\RegistrationSystem\\challange\\src\\registration\\" + concatenated + ".txt");
+        try(FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw)){
+            bw.write(user.getName() + "\n");
+            bw.write(user.getEmail() + "\n");
+            bw.write(user.getAge() + "\n");
+            String height = String.valueOf(user.getHeight());
+            bw.write(height);
+            bw.flush();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
