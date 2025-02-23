7280ad87bfe92e513d3c5dd62903033a1e421a1d
Ítalo Santana <italoeugenio539@gmail.com>
2025-02-23 18:45:39 -0300

    improve things

--- challange/src/form/FormWriterAndRemover.java
+++ challange/src/form/FormWriterAndRemover.java
@@ -91,7 +91,6 @@ public class FormWriterAndRemover {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        FormReader.formReader();
         System.out.println();
     }
 }
